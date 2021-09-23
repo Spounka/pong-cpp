ae70b2bfc075c1915c802cc81623c4ff854602c4
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2021-09-23 21:14:59 +0100

    added ball physics and resource initialisation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <SFML/Graphics.hpp>
 
 #include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
 
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
@@ -8,6 +11,27 @@
 #define FRAME_RATE 60U
 
 #define Move_Speed 10.0f
+#define FONT "/home/spounka/.fonts/TTF/consolaz.ttf"
+
+static bool gameStarted = false;
+static sf::Font font;
+static sf::Text gameText;
+static sf::Vector2f ballDirection;
+
+bool initResources() {
+    if (!font.loadFromFile(FONT)) {
+        std::cerr << "Failed to load font, exiting" << std::endl;
+        return false;
+    }
+    gameText.setFont(font);
+    gameText.setFillColor(sf::Color::White);
+    gameText.setString(L"W / S to Move left side\n"
+                       L"Up / Down to move right side\n "
+                       L"Space to start game");
+    gameText.setPosition(
+        WINDOW_WIDTH / 2.f - gameText.getGlobalBounds().width / 2, 10);
+    return true;
+}
 
 void initRect(sf::RectangleShape &rect, const sf::Vector2f &&position,
               const sf::Color &color = sf::Color::White, const float rot = 0) {
@@ -30,7 +54,6 @@ void initBall(sf::CircleShape &circle, const sf::Vector2f &&position,
 
 void initWindow(sf::RenderWindow &window, const char *title,
                 sf::Uint32 style = 7U) {
-
     window.create(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), title, style);
     window.setVerticalSyncEnabled(true);
     window.setFramerateLimit(FRAME_RATE);
@@ -49,9 +72,43 @@ void moveRect(sf::RectangleShape &rect, const sf::Vector2f &&direction) {
     }
 }
 
+void checkBallCollision(sf::CircleShape &circle) {
+    auto radius = circle.getRadius();
+    auto position = circle.getPosition();
+    if (position.y - radius <= 0 || position.y + radius >= WINDOW_HEIGHT) {
+        ballDirection.y = -ballDirection.y;
+    }
+    if (position.x - radius <= 0 || position.x + radius >= WINDOW_WIDTH) {
+        ballDirection.x = -ballDirection.x;
+    }
+}
+
+void moveBallRandomPos() {
+    float randomX =
+        static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX);
+    float randomY =
+        static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX);
+    float magnitude = std::sqrt(randomX * randomX + randomY * randomY);
+    ballDirection = sf::Vector2f(randomX / magnitude, randomY / magnitude);
+}
+
+void initGame() {
+    if (!gameStarted && sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+        gameStarted = true;
+        gameText.setString(L"");
+        moveBallRandomPos();
+    }
+}
+
+void moveBall(sf::CircleShape &circle) {
+    circle.move(ballDirection * Move_Speed);
+}
+
 void handleRectInput(sf::RectangleShape &leftRect,
                      sf::RectangleShape &rightRect) {
 
+    if (!gameStarted)
+        return;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
         moveRect(leftRect, sf::Vector2f(0, 1.f));
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
@@ -62,12 +119,9 @@ void handleRectInput(sf::RectangleShape &leftRect,
         moveRect(rightRect, sf::Vector2f(0, -1.f));
 }
 
-void handleRightRectInput(sf::RectangleShape &rect) {}
-
 int main(int argc, char **argv) {
 
-    printf("size of pointer: %lu\n", sizeof(sf::Vector2f *));
-
+    srand(static_cast<unsigned>(time(0)));
     sf::RenderWindow window;
     initWindow(window, "S::Pong", sf::Style::Default ^ sf::Style::Resize);
 
@@ -88,18 +142,26 @@ int main(int argc, char **argv) {
     initBall(ball, sf::Vector2f(WINDOW_WIDTH / 2.f, WINDOW_HEIGHT / 2.f),
              sf::Color::White, size_t(ballVertCount));
 
+    if (!initResources())
+        return (EXIT_FAILURE);
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
+            initGame();
             handleRectInput(left, right);
         }
+
+        moveBall(ball);
+        checkBallCollision(ball);
 
         window.clear();
         window.draw(left);
         window.draw(right);
         window.draw(ball);
+        window.draw(gameText);
         window.display();
     }
 
